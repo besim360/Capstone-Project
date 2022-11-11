9dc8c28a1af78aafdf1f3c73a2a2a506e4c33a22
Derek Olson <dereko@derekolsondev.com>
2022-11-10 17:06:04 -0800

    fixing top styles

--- frontend/src/layouts/MainLayout.vue
+++ frontend/src/layouts/MainLayout.vue
@@ -1,15 +1,22 @@
 <template>
   <q-layout view="hHh LpR fFf">
 
-    <q-header reveal elevated class="bg-primary text-white">
+    <q-header reveal elevated class="bl-30 text-dark pl-0">
+      <div class="color-strip"></div>
       <q-toolbar>
-
+        <div class="bg-accent logo-bg">
+          <img src="../assets/Icons/branding-iconography/wsu-icon.svg" class="center-aligned">
+        </div>
         <q-toolbar-title>
-          <q-avatar>
-            <img src="../assets/Icons/branding-iconography/wsu-icon.svg">
-          </q-avatar>
+
           Bibliography Application
         </q-toolbar-title>
+        <q-space />
+        <q-tabs v-model="tab" shrink>
+          <q-tab name="search" label="Search" />
+          <q-tab name="bibliography" label="Bibliography" />
+          <q-tab name="upload" label="Upload" />
+        </q-tabs>
       </q-toolbar>
     </q-header>
 
@@ -37,3 +44,29 @@ export default {
   }
 }
 </script>
+
+<style lang="scss" scoped>
+.bl-30 {
+  background-color: $bl-30
+}
+
+.color-strip {
+  background-color: $primary;
+  width: 100%;
+  height: 8px;
+}
+
+.logo-bg {
+  width: 56px;
+  height: 56px;
+  text-align: center;
+}
+
+.center-aligned {
+  position: relative;
+  top: 50%;
+  -ms-transform: translateY(-50%);
+  -webkit-transform: translateY(-50%);
+  transform: translateY(-50%);
+}
+</style>
