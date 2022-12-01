37f7c2c324527cc0f834b31cb8a7c371559b0641
Danf96 <danielthomasf@gmail.com>
2022-12-01 13:10:14 -0800

    Spring Boot initial setup.

---
+++ middle/javapi/src/main/java/com/springapi/javapi/JavapiApplication.java
@@ -0,0 +1,13 @@
+package com.springapi.javapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavapiApplication.class, args);
+	}
+
+}
