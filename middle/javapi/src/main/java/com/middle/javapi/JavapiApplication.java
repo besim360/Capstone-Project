fcd5bb6e91bc1566c8f9a11d7474085906f2f099
Danf96 <danielthomasf@gmail.com>
2022-12-02 11:06:57 -0800

    Additional Hibernate setup.

--- middle/javapi/src/main/java/com/middle/javapi/JavapiApplication.java
+++ middle/javapi/src/main/java/com/middle/javapi/JavapiApplication.java
@@ -1,4 +1,4 @@
-package com.springapi.javapi;
+package com.middle.javapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- middle/javapi/src/test/java/com/middle/javapi/JavapiApplicationTests.java
+++ middle/javapi/src/test/java/com/middle/javapi/JavapiApplicationTests.java
@@ -1,4 +1,4 @@
-package com.springapi.javapi;
+package com.middle.javapi;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
