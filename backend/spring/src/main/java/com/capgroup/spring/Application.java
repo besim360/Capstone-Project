08fa497bde779ebf6cf817f32684718b741833ba
Gabriel Reed <gabriel.s.reed@wsu.edu>
2023-01-16 12:40:55 -0800

    Add extra example articles

--- backend/spring/src/main/java/com/capgroup/spring/Application.java
+++ backend/spring/src/main/java/com/capgroup/spring/Application.java
@@ -12,6 +12,8 @@ import org.springframework.context.annotation.Bean;
 import java.util.Arrays;
 import java.util.List;
 
+// http://localhost:9000/article/search?text=query&limit=5
+
 @SpringBootApplication
 public class Application {
 
@@ -42,7 +44,10 @@ public class Application {
 			article2.setStartYear(2000);
 			article2.setEndYear(2000);
 			article2.setSubjectCodes("d60, d4, b62");
-			article2.setTopics("web page construction, web design, intranet design, online, usability, usability testing, users, user-centered, UX, focus groups, ergonomics, human factors, legal writing, laws, legalities, contracts, letters of agreement, regulations, censorship, lawyers, law enforcement, police, crime, dishonesty, genocide, society, editing");
+			article2.setTopics("web page construction, web design, intranet design, online, usability, usability " +
+					"testing, users, user-centered, UX, focus groups, ergonomics, human factors, legal writing, laws, " +
+					"legalities, contracts, letters of agreement, regulations, censorship, lawyers, law enforcement, " +
+					"police, crime, dishonesty, genocide, society, editing");
 
 			Article article3 = new Article();
 			article3.setTitle("His Master's Voice: Tiro and the Rise of the Roman Secretarial Class");
@@ -56,8 +61,47 @@ public class Application {
 			article3.setPages("155-168");
 			article3.setDoi("10.2190/B4YD-5FP7-1W8D-V3UC");
 			article3.setSubjectCodes("b49, c572, c649");
-			article3.setTopics("history of technical communication, history of writing, history, personal names, cicero, rome, roman, place names");
-			List<Article> articles = Arrays.asList(article1, article2, article3);
+			article3.setTopics("history of technical communication, history of writing, history, " +
+					"personal names, cicero, rome, roman, place names");
+
+
+			Article article4 = new Article();
+			article4.setTitle("Comparing Cultural Perceptions of Editing from the Author's Point of View");
+			article4.setAuthors( "Eaton, Angela, Pamela Estes Brewer, Tiffany Craft Portewig, and Cynthia R. Davidson");
+			article4.setVolNum("55.2");
+			article4.setSourceAbbrev("TC(STC)");
+			article4.setSourceLong("Technical Communication");
+			article4.setDate("May");
+			article4.setStartYear(2008);
+			article4.setEndYear(2008);
+			article4.setPages("140-166");
+			article4.setDoi(null);
+			article4.setSubjectCodes("b34, b129, b2, b173");
+			article4.setTopics("book reviews: written, internationalism, intercultural issues, transnational, diversity, " +
+					"culture, localization, localizing, immigration, migrants, globalism, multiculturalism, bilingualism, " +
+					"multilingualism, universal, composition of themes, gender, women-men, feminism, transgender, queer, masculinity, sexism");
+
+			Article article5 = new Article();
+			article5.setTitle("Copyright, Free Speech, and Democracy: Eldred v. Ashcroft and Its Implications for Technical Communicators");
+			article5.setAuthors( "Herrington, TyAnna");
+			article5.setVolNum("20.1");
+			article5.setSourceAbbrev("TechCommQ");
+			article5.setSourceLong("Technical Communication Quarterly");
+			article5.setDate(null);
+			article5.setStartYear(2010);
+			article5.setEndYear(2010);
+			article5.setPages("47-72");
+			article5.setDoi("10.1080/10572252.2011.528321");
+			article5.setSubjectCodes("d49, b62, b164");
+			article5.setTopics("copyright, intellectual property, fair use, plagiarism, patents, trademarks, " +
+					"information access, open access, legal writing, laws, legalities, contracts, letters of agreement," +
+					" regulations, censorship, lawyers, law enforcement, police, crime, dishonesty, genocide, technology " +
+					"transfer, diffusion, knowledge transfer, knowledge sharing, information-transfer model, boundary " +
+					"spanning, boundary spanner");
+
+
+
+			List<Article> articles = Arrays.asList(article1, article2, article3, article4, article5);
 			articleRepository.saveAll(articles);
 		};
 	}
