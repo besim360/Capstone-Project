9605851823a80aebf712127738740984f668a80f
AV3T <73491566+AV3T@users.noreply.github.com>
2023-03-29 08:06:19 -0700

    latest updates

--- middle/routes/keycloak.py
+++ middle/routes/keycloak.py
@@ -1,44 +1,57 @@
-from fastapi import APIRouter
+from fastapi import FastAPI, Request
 import httpx
 import json
-keycloak = APIRouter()
+import uvicorn
+import jwt
+
+app = FastAPI()
 
 #Authenticate the user with username and password, and receive their bearer token.
-@keycloak.post("/authenticate-user")
-def authenticateUser (username : str, password : str):
+@app.post("/authenticate-user/")
+def authenticateUser (email : str, password : str):
     request = httpx.post("http://localhost:8080/realms/cs420/protocol/openid-connect/token", 
-    data={'username' : username, 
+    data={'username' : email, 
         'password' : password, 
         'grant_type' : 'password', 
         'client_id' : 'rest-client'})
     data = json.loads(request.text)
+    bearer_token = data["access_token"]
+    public_key = "-----BEGIN PUBLIC KEY-----\nMIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAxSR112675T/+eZBYIi5q/OCKfhMGTmA9VjMEjDHCaG/eKxmCLQUQXNtS3yNvCBQG/L55eIS4GwmYPYgj0FW3eQeu6A+KAJuSsLvQTRlEKgdgOqRl7j7c29yuShr1zYEQVfUr4y4XLbAU2WQ+y5EQU3VmNO9ebgFWBEjwWQtdLgSlgaQg4JdlaWvKQIFxzd9mJcC4YmYnUir+q46lh8RUDUglqwoTQmSTnkZ3nIn/N+SQM8L5l9d4/o9FMo+FDXVXjzoDAqMTDWPrCy02sTZKiLslCxkW1enPB+BBDJ4o+XCcbaOD5Twd/ON7YZ3IpQRsiLS8CcYSEZCE8LvUrH1hRQIDAQAB\n-----END PUBLIC KEY-----"
+    decoded_bearer_token = jwt.decode(bearer_token, public_key, audience="account",algorithms="RS256")
+    
+    #uncomment below for extracted user ID 
+    #decoded_bearer_token["sub"]
+    
     return data
 
 
 #Retrieve all current users.
-@keycloak.get("/retrieve-all-users")
-def getCurrentUsers(token : str):
+@app.get("/retrieve-all-users/")
+def getCurrentUsers(request: Request):
+    token = request.headers.get('Authorization')
     request = httpx.get("http://localhost:8080/admin/realms/cs420/users", 
     headers= {'Authorization': 'Bearer ' + token})
-    request = json.loads(request.text)
-    return request
+    text = json.loads(request.text)
+    return request.status_code, text
 
 
 #Retrieve specified user based on their email, and return their Keycloak ID.
-@keycloak.get("/retrieve-user")
-def retrieveUser(email : str, token : str):
+@app.get("/retrieve-user/")
+def retrieveUser(email : str, request: Request):
+    token = request.headers.get('Authorization')
     request = httpx.get("http://localhost:8080/admin/realms/cs420/users",
     headers={'Authorization': 'Bearer ' + token},
     params={'email' : email})
-    request = json.loads(request.text)
-    return request
+    text = json.loads(request.text)
+    return request.status_code, text
 
 
 #Set the password for specified user (forgot password functionality).
-@keycloak.put("/set-password")
-def setUserPassword(email : str, newPassword : str, token : str):
+@app.put("/set-password/")
+def setUserPassword(email : str, newPassword : str, request : Request):
+    token = request.headers.get('Authorization')
     userObject = retrieveUser(email, token)
-    userid = userObject[0]["id"]
+    userid = userObject[1][0]["id"]
     request = httpx.put("http://localhost:8080/admin/realms/cs420/users/"+userid+"/reset-password",
     headers={'Authorization': 'Bearer ' + token},
     json={"value":newPassword})
@@ -46,8 +59,9 @@ def setUserPassword(email : str, newPassword : str, token : str):
 
 
 #Create a user with specified paramaters.
-@keycloak.post("/create-user")
-def createUser(email : str, firstName : str, lastName : str, password : str, token : str):
+@app.post("/create-user/")
+def createUser(email : str, firstName : str, lastName : str, password : str):
+    token = request.headers.get('Authorization')
     request = httpx.post("http://localhost:8080/admin/realms/cs420/users",
     headers={'Authorization': 'Bearer ' + token},
     json={"email": email,
@@ -63,22 +77,48 @@ def createUser(email : str, firstName : str, lastName : str, password : str, tok
     return request.status_code
 
 
-# #Delete specified user account.
-@keycloak.delete("/delete-user")
-def deleteUser(email : str, token : str):
+#Delete specified user account.
+@app.delete("/delete-user/")
+def deleteUser(email : str, request : Request):
+    token = request.headers.get('Authorization')
     userObject = retrieveUser(email, token)
-    userid = userObject[0]["id"]
+    userid = userObject[1][0]["id"]
     request = httpx.delete("http://localhost:8080/admin/realms/cs420/users/"+userid,
     headers={'Authorization': 'Bearer ' + token})
     return request.status_code
     
 
-#Function to promote a regular user to Admin user - to implement.
-@keycloak.post("/promote-user")
-def promoteUser(email : str, token : str):
-    userID = retrieveUser(email, token)
-    request = httpx.post("http://localhost:8080/admin/realms/cs420/users/"+userID+"/role-mkeycloakings/realm",
-    headers={'Authorization': 'Bearer ' + token},
-    json={"id": "roleId,",
-        "name": "roleName"})
+#Function to promote a regular user to Admin user.
+@app.post("/promote-user/")
+def promoteUser(email : str, request : Request):
+    token = request.headers.get('Authorization')
+    userObject = retrieveUser(email, token)
+    userid = userObject[1][0]["id"]
+    request = httpx.put("http://localhost:8080/admin/realms/cs420/users/"+userid+"/groups/1261360f-6256-4ca2-bbc2-a1f07ceef150",
+    headers={'Authorization': 'Bearer ' + token})
     return request.status_code
+
+
+#Function to demote an Admin user to regular user.
+@app.post("/demote-user/")
+def demoteUser(email : str, request : Request):
+    token = request.headers.get('Authorization')
+    userObject = retrieveUser(email, token)
+    userid = userObject[1][0]["id"]
+    request = httpx.delete("http://localhost:8080/admin/realms/cs420/users/"+userid+"/groups/1261360f-6256-4ca2-bbc2-a1f07ceef150",
+    headers={'Authorization': 'Bearer ' + token})
+    return request.status_code
+
+
+#Change status of the user (enabled/disabled)
+@app.post("/change-user-status/")
+def changeStatus(email : str, request : Request, status : bool):
+    token = request.headers.get('Authorization')
+    userObject = retrieveUser(email, token)
+    userid = userObject[1][0]["id"]
+    request = httpx.put("http://localhost:8080/admin/realms/cs420/users/"+userid,
+    headers={'Authorization': 'Bearer ' + token}, json={"enabled":status})
+    return request.status_code
+
+if __name__ ==  "__main__":
+    uvicorn.run(app, port = 8000, host = "0.0.0.0")
